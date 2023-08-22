bff675e42c133ec09b9ad98ceeae0c12046982bf
meitav5 <meitav102@gmail.com>
2023-08-22 17:54:02 -0400

    project added.

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ setup(
     name='backend',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
+    install_requires=['awscli', 'pycryptodome', 'blinker', 'boto3', 'botocore', 'click', 'Flask', 'Flask-Cors', 'Flask-JWT-Extended', 'gunicorn', 'importlib-metadata', 'itsdangerous', 'Jinja2', 'jmespath', 'MarkupSafe', 'pyasn1', 'PyJWT', 'python-dateutil', 'python-dotenv', 'rsa', 's3transfer', 'six', 'urllib3', 'Werkzeug', 'zipp'],
     setup_requires=[
-        'pytest-runner',
+        'awscli', 'pycryptodome', 'blinker', 'boto3', 'botocore', 'click', 'Flask', 'Flask-Cors', 'Flask-JWT-Extended', 'gunicorn', 'importlib-metadata', 'itsdangerous', 'Jinja2', 'jmespath', 'MarkupSafe', 'pyasn1', 'PyJWT', 'python-dateutil', 'python-dotenv', 'rsa', 's3transfer', 'six', 'urllib3', 'Werkzeug', 'zipp', 'pytest-runner',
     ],
     tests_require=[
         'pytest',
